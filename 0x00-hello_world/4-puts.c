ab8822bd53ed4aee0be9c6bb16810264cf87ed0d
Isaacodengs <isaacodengle1998@gmail.com>
2022-10-13 06:05:28 -0700

    got

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - printing in c
  * return: Always 0 (good)
  */
 
 int main(void)
-
 {
     puts("\ "Programming is like building a multilingual puzzle");
              return (0);
-
 }    
