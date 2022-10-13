ac85b0bc10e9db9d15e56bd98bd37eaddd0b0ab9
Isaacodengs <isaacodengle1998@gmail.com>
2022-10-13 06:15:46 -0700

    store

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - printing in c
- * return: Always 0 (good)
+ *main - printing in c
+ *return: Always 0 (good)
  */
 
 int main(void)
 {
-    puts("\ "Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
              return (0);
 }    
