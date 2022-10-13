fb07ae331ee202df5ef043a460b625a20345092f
Isaacodengs <isaacodengle1998@gmail.com>
2022-10-13 06:57:04 -0700

    hide

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <unistd.h>
 
+
 /**
  * main - prints a text
  *
  * Return: Always 1 (success)
  */
+
 int main(void)
 {
          write(2, "all that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
