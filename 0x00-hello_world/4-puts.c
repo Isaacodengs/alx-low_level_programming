5732c794dd42f0f10544bab68e37f316c1c9b389
Isaacodengs <isaacodengle1998@gmail.com>
2022-10-15 07:02:27 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
- *main - printing in c
+ * main - Entry Point
  *
- *Return: Always 0 (good)
+ * Return: Always 0 (success)
  */
+
 int main(void)
 {
-       puts("\"Programming is like building a multilingual puzzle");
-                         return (0);
-}  
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
