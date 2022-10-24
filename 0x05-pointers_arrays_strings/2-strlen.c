d049777cf7069dcf5456b5a693dd6c317e8180ff
Isaacodengs <isaacodengle1998@gmail.com>
2022-10-24 06:55:06 -0700

    task1

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  *_strlen -length of a string
@@ -9,8 +9,7 @@ include "main.h"
 
 int _strlen(char *s)
 {
-int u:
-
+int u;
 u = 0;
 while (s[u] != '\0')
 {
